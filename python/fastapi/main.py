61359ca52f85631dbaaa5937de709bba00b5a156
Karan Kadam <karank@outlook.com>
2021-03-28 22:49:43 +1030

    small updates

--- python/fastapi/main.py
+++ python/fastapi/main.py
@@ -5,3 +5,8 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello FastAPI"}
+
+
+@app.get("/items/{item_id}")
+async def items(item_id: int):
+    return {"parameter_passed": item_id}
