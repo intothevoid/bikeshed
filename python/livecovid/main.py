8ebf946e8eaa8250bcb4045c46b1e7828fc5a4ff
Karan Kadam <karank@outlook.com>
2021-12-22 22:43:39 +1030

    added bootstrap and primitive ui

--- python/livecovid/main.py
+++ python/livecovid/main.py
@@ -1,9 +1,16 @@
 from scrapers.covid import CovidScraper
 from fastapi import FastAPI
+from starlette.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 covscrape = CovidScraper()
 
 @app.get('/numbers/{state}')
 async def get_numbers(state):
     return covscrape.get_covid_numbers(state)
+
+@app.get('/')
+async def get_root():
+    return FileResponse('index.html')
