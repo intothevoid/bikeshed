5291884acf03ef212a092bf461952736947d9905
Karan Kadam <karank@outlook.com>
2022-01-15 13:40:55 +1030

    hook data source

--- angular/ang-http/src/app/data.service.ts
+++ angular/ang-http/src/app/data.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
+  private REST_API_SERVER = 'http://localhost:3000';
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  public sendGetRequest() {
+    return this.httpClient.get(this.REST_API_SERVER);
+  }
 }
--- angular/ang-http/src/app/home/home.component.ts
+++ angular/ang-http/src/app/home/home.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  products = [];
 
-  constructor() { }
+  constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
+    this.dataService.sendGetRequest().subscribe((data: any) => {
+      console.log(data);
+      this.products = data;
+    });
   }
-
 }
