6a4c55560721594c29961892eaa3fa61974ce1f0
Karan Kadam <karank@outlook.com>
2022-01-18 22:27:38 +1030

    check if trees are same

--- golang/practice/binarytree.go
+++ golang/practice/binarytree.go
@@ -61,7 +61,25 @@ func Walk(t *tree.Tree, ch chan int) {
 // Same determines whether the trees
 // t1 and t2 contain the same values.
 func Same(t1, t2 *tree.Tree) bool {
-	return false
+	ch1, ch2 := make(chan int), make(chan int)
+
+	go WalkClosure(t1, ch1)
+	go WalkClosure(t2, ch2)
+
+	for {
+		v1, ok1 := <-ch1
+		v2, ok2 := <-ch2
+
+		if v1 != v2 || ok1 != ok2 {
+			return false
+		}
+
+		if !ok1 {
+			break
+		}
+	}
+
+	return true
 }
 
 func main() {
