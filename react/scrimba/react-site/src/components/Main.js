1a8620de226ebe55897614109500d8b4bafb377d
Karan Kadam <karank@outlook.com>
2022-10-21 14:53:51 +1030

    fix: fix issues with css and colors

--- react/scrimba/react-site/src/components/Main.js
+++ react/scrimba/react-site/src/components/Main.js
@@ -3,8 +3,10 @@ import React from "react";
 
 function Main() {
     return (
+      <div className="main">
         <h1>This is the main component!</h1>
-    )
+      </div>
+    );
 }
 
 export default Main;
--- react/scrimba/react-site/src/components/Navbar.js
+++ react/scrimba/react-site/src/components/Navbar.js
@@ -2,8 +2,10 @@ import React from "react";
 
 function NavBar() {
     return (
+      <div className="navbar">
         <h1>This is a Navbar!</h1>
-    )
+      </div>
+    );
 }
 
 export default NavBar;
