5655b168ae0c1706ea5ee53d3526d11189610850
Karan Kadam <karank@outlook.com>
2022-10-31 10:48:13 +1030

    feat: 🎸 added meme data store, pull ramdom meme images

--- react/scrimba/meme-generator/src/components/Meme.js
+++ react/scrimba/meme-generator/src/components/Meme.js
@@ -1,9 +1,22 @@
 import React from "react";
+import memesData from "../memesData";
+
+function updateMemeImage() {
+  const url = getRandomMemeUrl();
+  const memeImage = document.getElementById("meme");
+  memeImage.src = url;
+}
+
+function getRandomMemeUrl() {
+  const randomIndex = Math.floor(Math.random() * memesData.data.memes.length);
+  console.log("index" + randomIndex);
+  return memesData.data.memes.at(randomIndex).url;
+}
 
 function Meme() {
   return (
     <div className="meme">
-      <form>
+      <div>
         <div className="meme-top">
           <input className="meme-textbox" type="text" placeholder="Top Text" />
           <input
@@ -12,8 +25,16 @@ function Meme() {
             placeholder="Bottom Text"
           />
         </div>
-        <button className="meme-button">Get a new meme image 🖼</button>
-      </form>
+        <button className="meme-button" onClick={updateMemeImage}>
+          Get a new meme image 🖼
+        </button>
+      </div>
+      <img
+        id="meme"
+        src={getRandomMemeUrl()}
+        className="meme-img"
+        alt="Loading meme..."
+      />
     </div>
   );
 }
