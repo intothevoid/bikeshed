edc245ea6070d53bcb2cc256844cbaf62d5b26af
Karan Kadam <karank@outlook.com>
2023-05-05 13:29:15 +0930

    fix: 🐛 fix issue with disk space check

--- python/motogp_dl/app.py
+++ python/motogp_dl/app.py
@@ -1,13 +1,17 @@
+import sys
+import psutil
 import contextlib
 import logging
 import feedparser
 import re
 import subprocess
 import time
+import logging
 
 # Settings
 FEED = feedparser.parse("https://www.reddit.com/r/MotorsportsReplays.rss")
-DOWNLOAD_DIR = "/downloads"
+DOWNLOAD_TYPES = ["motogp"]  # example: ["motogp", "formula 1"]
+DOWNLOAD_DIR = "./downloads"
 QUALITY = "1080"
 INTERVAL_MINS = 60  # minutes
 DELETE_OLD_FILES = True
@@ -17,11 +21,17 @@ DELETE_OLD_FILES_THRESHOLD = 10  # GB
 logging.basicConfig(
     filename=f"app.log",
     filemode="a",
-    format="%(asctime)s - %(message)s",
+    format="%(asctime)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 )
 
-LOGGER = logging.getLogger(__name__)
+# add console handler
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setLevel(logging.INFO)
+console_handler.setFormatter(
+    logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+)
+logging.getLogger("").addHandler(console_handler)
 
 
 # Iterate over the entries and extract magnet links
@@ -35,39 +45,45 @@ def parse_feed(latest: bool = True):
      Returns:
         None
     """
+    logging.info(
+        f"Searching for downloads of type(s): {DOWNLOAD_TYPES} - Quality: {QUALITY}"
+    )
     for entry in FEED.entries:
-        if "motogp" in entry.title.lower() and (
+        logging.info(f"Checking entry: {entry.title}")
+        if any(dl_type in entry.title.lower() for dl_type in DOWNLOAD_TYPES) and (
             f"{QUALITY}" in entry.title.lower() or "hd" in entry.title.lower()
         ):
             # Extract magnet link from content
             match = re.search(r"magnet:\?xt=urn:btih:\w+", entry.content[0].value)
             if match:
                 magnet_link = match.group(0)
-                LOGGER.info(f"Found magnet link:{entry.title} - {magnet_link}")
+                logging.info(f"Found magnet link:{entry.title} - {magnet_link}")
 
                 if already_downloaded(magnet_link):
-                    LOGGER.info(f"Already downloaded - skipping: {magnet_link}")
+                    logging.info(f"Already downloaded - skipping: {magnet_link}")
                     continue
 
                 # check disk space, delete oldest file if below threshold until above threshold
                 while is_disk_space_below_threshold(DELETE_OLD_FILES_THRESHOLD):
-                    LOGGER.warning(f"Disk space below threshold - deleting oldest file")
+                    logging.warning(
+                        f"Disk space below threshold - deleting oldest file"
+                    )
                     delete_oldest_file()
 
                     time.sleep(15)  # sleep for 15 seconds, wait for file to be deleted
 
                 try:
                     # Pass magnet link to aria2 via command line
-                    LOGGER.info(f"Downloading: {magnet_link} via aria2")
+                    logging.info(f"Downloading: {magnet_link} via aria2")
                     ret = run_aria2c(magnet_link)
                     # process return code
                     if ret.returncode == 0:
-                        LOGGER.info(f"Downloaded: {magnet_link}")
+                        logging.info(f"Downloaded: {magnet_link}")
                     else:
-                        LOGGER.error(f"Error downloading: {magnet_link}")
+                        logging.error(f"Error downloading: {magnet_link}")
                         continue
                 except FileNotFoundError as exc:
-                    LOGGER.error(f"aria2c not found {exc}")
+                    logging.error(f"aria2c not found {exc}")
                     if latest:
                         break
                     else:
@@ -81,8 +97,6 @@ def parse_feed(latest: bool = True):
                 # break out if we have downloaded the latest
                 if latest:
                     break
-
-            LOGGER.info(f"Downloaded {magnet_link}")
 
 
 def run_aria2c(magnet_link):
@@ -125,19 +139,14 @@ def already_downloaded(magnet_link: str) -> bool:
     return False
 
 
-def is_disk_space_below_threshold(threshold: int = DELETE_OLD_FILES_THRESHOLD) -> bool:
+def is_disk_space_below_threshold(threshold: int = DELETE_OLD_FILES_THRESHOLD):
     """This function checks the disk space of the download directory.
     If the disk space is less than 10GB it will delete the oldest file in the directory.
     """
-    # get disk space
-    df = subprocess.run(["df", "-h", f"{DOWNLOAD_DIR}"], capture_output=True)
-    disk_space = df.stdout.decode("utf-8").split("\n")[1].split()[3]
-
-    # check if disk space is less than threshold
-    if int(disk_space[:-1]) < threshold:
-        return True
-
-    return False
+    disk_usage = psutil.disk_usage("/")
+    disk_space = f"{disk_usage.free / (2**30):.1f} GB"  # calculate free space in GB and format as string
+    free_space_in_gb = float(disk_space[:-3])
+    return int(free_space_in_gb) < threshold
 
 
 def delete_oldest_file():
@@ -159,6 +168,8 @@ def delete_oldest_file():
 
 # Call parse_feed() every INTERVAL_MINS minutes
 if __name__ == "__main__":
+    logging.info("Starting MotoGP Downloader v1.0")
     while True:
+        logging.info("Parsing reddit.com/r/MotorsportsReplays feed for new content")
         parse_feed(True)
         time.sleep(60 * INTERVAL_MINS)  # interval in mins
