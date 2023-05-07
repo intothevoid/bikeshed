338230dbdf9fd47a09cd21f1bc843aabc725e43a
Karan Kadam <karank@outlook.com>
2023-05-07 13:06:04 +0930

    fix: 🐛 better error handling

--- python/motogp_dl/gotify/message.py
+++ python/motogp_dl/gotify/message.py
@@ -16,7 +16,14 @@ def send_gotify_message(message: str, title: str = "motogp_dl"):
     gotify_app_id = os.environ.get("GOTIFY_APP_ID")
 
     # check if gotify token, url and app id are set
-    if gotify_token is None or gotify_url is None or gotify_app_id is None:
+    if (
+        gotify_token is None
+        or gotify_url is None
+        or gotify_app_id is None
+        or gotify_token == ""
+        or gotify_url == ""
+        or gotify_app_id == ""
+    ):
         LOGGER.error(
             "GOTIFY_TOKEN, GOTIFY_URL and GOTIFY_APP_ID environment variables must be set"
         )
@@ -26,9 +33,14 @@ def send_gotify_message(message: str, title: str = "motogp_dl"):
 
     # send message to gotify
     try:
-        requests.post(
+        resp = requests.post(
             f"{gotify_url}/message?token={gotify_token}&app={gotify_app_id}",
             json=payload,
         )
+
+        if resp.status_code != 200:
+            LOGGER.error(
+                f"Error sending message to gotify: {resp.status_code} - {resp.text}. Details: {gotify_app_id} - {gotify_token} - {gotify_url}"
+            )
     except requests.exceptions.ConnectionError as exc:
         LOGGER.error(f"Error sending message to gotify: {exc}")
---
+++ python/motogp_dl/scripts/container-test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Once you have built the docker image, this script can be used to run the container.
+# This command will create a Docker container with the image 'motogp-dl' and name the container 'motogp-dl'. 
+# It will restart the container unless stopped and will also expose port 6911. 
+# It also creates a volume to store any downloads in the current directory.
+
+# create downloads directory if it doesn't exist, give it write permissions with existing user and group
+mkdir -p downloads
+chmod 777 downloads
+
+# run the container
+docker run --rm -it \
+  --name motogp-dl \
+  -p 6911:6911 -p 6881-6885:6881-6885 \
+  -v ./downloads:/downloads \
+  --env-file=./scripts/gotify.env \
+  motogp-dl:latest
